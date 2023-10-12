dde1ee9afa0686b79942f1acbdde65522d676473
rmunate <raulunatecastro@MacBook-Pro-de-Raul.local>
2023-10-12 09:18:05 -0500

    Cache

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -4,21 +4,22 @@ namespace HorizonFramework\Core;
 
 class Application
 {
-    public static function start($base_path, $time) {
-        return new self($base_path, $time);
-    }
-
-    private $basePath;
-    private $startTime;
-
-    public function __construct($base_path, $time) {
-        $this->basePath = $base_path;
-        $this->startTime = $time;
-    }
-
-    public function singleton($class)
+    public static function start($baseRoot, $starTime)
     {
-        $class::mount($this->basePath);
+        return new self($baseRoot, $starTime);
     }
-    
+
+    private $baseRoot;
+    private $starTime;
+
+    public function __construct($baseRoot, $starTime)
+    {
+        $this->baseRoot = $baseRoot;
+        $this->starTime = $starTime;
+    }
+
+    public function singleton($instance)
+    {
+        $kernel = $instance::mount($this->baseRoot, $this->starTime);
+    }
 }
--- src/Core/CLI/Kernel.php
+++ src/Core/CLI/Kernel.php
@@ -2,25 +2,44 @@
 
 namespace HorizonFramework\Core\CLI;
 
+use Dotenv\Dotenv;
+
 class Kernel
 {
     private static $instance;
 
-    private function __construct($basePath)
+    private function __construct($baseRoot, $starTime)
     {
-        $app = require_once $basePath . "/config/app.php";
+        //1. Cargar el entorno.
+        $dotenv = Dotenv::createImmutable($baseRoot);
+        $dotenv->load();
 
-        print_r($app);
-        
+        if (! file_exists($baseRoot . "/bootstrap/cache/config.php")) {
+
+            //2. Leer la configuracion
+            $config = require_once $baseRoot ."/config/app.php";
+    
+            //3. crear arreglo definitivo
+            $config = array_merge([
+                'base_root' => $baseRoot,
+                'start_app' => $starTime,
+            ], $config);
+            
+            //4. Crear Cache
+            $cache = "<?php return " . var_export($config, true) . ";";
+            $fileCache = fopen($baseRoot . "/bootstrap/cache/config.php", 'w');
+            fwrite($fileCache, $cache);
+            fclose($fileCache);
+        }
+       
     }
 
-    public static function mount($basePath)
+    public static function mount($baseRoot, $starTime)
     {
         if (self::$instance === null) {
-            self::$instance = new self($basePath);
+            self::$instance = new self($baseRoot, $starTime);
         }
+
         return self::$instance;
     }
-
-        
 }
